da14c08a2e6c428d5ca641ed7e1ff918a0cba360
root <root@KIK.localdomain>
2023-11-16 19:01:28 +0800

    cpu module construct

--- riscv/src/regFile.v
+++ riscv/src/regFile.v
@@ -1,4 +1,8 @@
-module regFile(clk);
+module regFile(clk, rdSig, rtSig, regId, modifydata, readData);
     input wire clk;
-
+    input wire rdSig;
+    input wire rtSig;
+    input wire regId;
+    input wire[31:0] modifyData;
+    output reg[31:0] readData;    
 endmodule
